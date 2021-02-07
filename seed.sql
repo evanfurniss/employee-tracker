100b0a3562832bd384cbafe38dbf835d8b3e44f7
Evan Furniss <evanfurniss@gmail.com>
2021-02-07 12:26:42 -0600

    created seed file

---
+++ seed.sql
@@ -0,0 +1,10 @@
+USE employees_db;
+
+INSERT INTO department (name) VALUES ("Management");
+INSERT INTO department (name) VALUES ("Marketing");
+INSERT INTO department (name) VALUES ("Sales");
+INSERT INTO department (name) VALUES ("Accounting");
+INSERT INTO department (name) VALUES ("IT");
+INSERT INTO department (name) VALUES ("Human Resources");
+INSERT INTO department (name) VALUES ("Research and Development");
+
